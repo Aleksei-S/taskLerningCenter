2bc273f077ac5b8d996b351d1598d5b779d74b83
Aleksei-S <mz-43@mail.ru>
2019-04-26 11:50:03 +0300

    first commit

---
+++ src/main/java/Validation/ValidationTaskMain.java
@@ -0,0 +1,16 @@
+package Validation;
+
+public class ValidationTaskMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Start main thread.");
+        System.out.println("Start main thread.");
+
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+
+        GoogleHelper.callDocument();
+
+        SalesforceHepler.checkUsersResults();
+    }
+}
